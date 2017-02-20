01f4bb55af6d397f5f3f42bb1da3f93cdb20152d
ksmai <kasingmai@gmail.com>
2017-02-20 23:52:00 +0800

    Set up tools for client-side development

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@ const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const paths = require('./gulp.config.json');
 const jasmine = require('gulp-jasmine');
+const minifyHTML = require('gulp-minify-html');
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const del = require('del');
+const babel = require('gulp-babel');
 
 gulp.task('lint-server', function() {
   return gulp.
@@ -17,5 +24,65 @@ gulp.task('test-server', ['lint-server'], function() {
     pipe(jasmine());
 });
 
-gulp.task('lint', ['lint-server']);
+gulp.task('lint-client', function() {
+  return gulp.
+    src(paths.clientJs).
+    pipe(eslint()).
+    pipe(eslint.format()).
+    pipe(eslint.failAfterError());
+});
+
+gulp.task('minhtml', function() {
+  return gulp.
+    src(paths.clientHtml).
+    pipe(minifyHTML({
+      empty: true
+    })).
+    pipe(gulp.dest(paths.bin));
+});
+
+gulp.task('mincss', function() {
+  return gulp.
+    src(paths.clientCss).
+    pipe(concat('styles.min.css')).
+    pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    })).
+    pipe(cleanCSS()).
+    pipe(gulp.dest(paths.bin));
+});
+
+gulp.task('minjs', ['lint-client'], function() {
+  return gulp.
+    src(paths.clientAppJs).
+    pipe(concat('app.min.js')).
+    pipe(babel({
+      presets: ['es2015']
+    })).
+    pipe(uglify()).
+    pipe(gulp.dest(paths.bin));
+});
+
+gulp.task('minvendor', function() {
+  return gulp.
+    src(paths.vendorJs).
+    pipe(concat('vendors.min.js')).
+    pipe(gulp.dest(paths.bin));
+});
+
+gulp.task('clean', function(cb) {
+  del([paths.bin], cb);
+});
+
+gulp.task('build', ['minhtml', 'mincss', 'minjs', 'minvendor']);
+gulp.task('lint', ['lint-server', 'lint-client']);
 gulp.task('test', ['test-server']);
+
+gulp.task('watch', ['build'], function() {
+  gulp.watch(paths.clientHtml, ['minhtml']);
+  gulp.watch(paths.clientCss, ['mincss']);
+  gulp.watch(paths.clientAppJs, ['minjs']);
+
+  console.log(`Watching on PID ${process.pid}`);
+});
